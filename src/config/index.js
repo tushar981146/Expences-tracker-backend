0632d24b79b295ffe64661a450903373e1fa53f4
tushar981146 <tusharblackhathacker@gmail.com>
2026-06-25 22:13:59 +0530

    cors origin fix

--- src/config/index.js
+++ src/config/index.js
@@ -22,7 +22,9 @@ const config = {
     },
 
      cors: {
-        origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+        origin: process.env.FRONTEND_URL
+            ? process.env.FRONTEND_URL.split(',').map(url => url.trim())
+            : ['http://localhost:5173'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     }
